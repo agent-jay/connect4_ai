d29018ac5736d92f4505ffc0d5afd5e84a2b2fec
sidjayam <sidjayam@umail.iu.edu>
2016-09-28 21:03:24 -0400

    Added case for perft when board fills up. Important check. Replaced incorrect negamax with working minimax

--- search.py
+++ search.py
@@ -8,6 +8,8 @@ def perft(board, depth):
         return 1
     if board.last_move_won():
         return 1
+    if board.is_full():
+        return 1
     count=0
     for move in board.generate_moves():
         board.make_move(move)
@@ -15,56 +17,43 @@ def perft(board, depth):
         board.unmake_last_move()
     return count
 
-state_val_lookup={} #should be refreshed every move
 
-def negamax(board,depth):
+    
+def find_win(board, depth):
+    values=[]
+    for move in board.generate_moves():
+        board.make_move(move)
+        values.append((minimax(board,depth-1,maxplayer=False),move))
+        board.unmake_last_move()
+    minimax_value=max(values)
+    if minimax_value[0]>0:
+        # print("WIN BY PLAYING %s"% minimax_value[1])
+        return "WIN BY PLAYING %s"% minimax_value[1]
+    elif minimax_value[0]<0:
+        # print("ALL MOVES LOSE")
+        return "ALL MOVES LOSE"
+    else:
+        # print("NO FORCED WIN IN %s MOVES"%depth)
+        return "NO FORCED WIN IN %s MOVES"%depth
+
+def minimax(board,depth, maxplayer):
     if board.last_move_won():
-        if board.last_player==1:
+        if maxplayer==True:
             return -100
         else :
             return +100
     
-    if depth==0:
-        return 0 #Why 0 as an evaluation
+    if depth==0 or board.is_full():
+        return 0 
 
     values=[]
     for move in board.generate_moves():
         board.make_move(move)
-        #str_board= str(board)
-        # if str_board in state_val_lookup:
-            # values.append(state_val_lookup[str_board])
-        # else:       
-        val=-negamax(board,depth-1)
-        values.append(val)
-        #state_val_lookup[str_board]=val
+        values.append(minimax(board,depth-1, not maxplayer))
         board.unmake_last_move()
-    return max(values)
-
-    
-def find_win(board, depth):
-    if board.last_move_won():
-        if board.last_player==1:
-            return "PLAYER 1 ALREADY WON"
-        else:
-            return "PLAYER 2 ALREADY WON"
-    values=[]
-    for move in board.generate_moves():
-        board.make_move(move)
-        values.append((-negamax(board,depth-1), move))
-        board.unmake_last_move()
-    minimax_value=max(values)
-    if minimax_value[0]>0:
-        print("WIN BY PLAYING %s"% minimax_value[1])
-        return "WIN BY PLAYING %s"% minimax_value[1]
-    elif minimax_value[0]<0:
-        print("ALL MOVES LOSE")
-        return "ALL MOVES LOSE"
+    #If maximizing player
+    if maxplayer==True:
+        return max(values)
+    #if minimizing player
     else:
-        print("NO FORCED WIN IN %s MOVES"%depth)
-        return "NO FORCED WIN IN %s MOVES"%depth
-
-b=board.Board()
-start=time.clock()
-#print(perft(b,8))
-#find_win(b,8)
-print(time.clock()-start)
+        return min(values)
