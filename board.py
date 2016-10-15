06264e818ed08c8fd809223bb0bea028457c59d7
sidjayam <sidjayam@umail.iu.edu>
2016-10-15 15:26:26 -0400

    New last move won- simpler, faster

--- board.py
+++ board.py
@@ -4,6 +4,7 @@ class Board:
         self.move_hist= []
         self.last_player= 2
         self.head=[6 for x in range(7)] #position of highest elements
+        self.total_moves=0
 
     def depth(self):
         return len(self.move_hist)
@@ -25,6 +26,7 @@ class Board:
         self.move_hist.append((self.head[move],move))
         self.state[self.head[move]][move]= -self.last_player+3
         self.last_player=-self.last_player+3
+        self.total_moves+=1
         
     def unmake_last_move(self):
         if len(self.move_hist)==0:
@@ -34,51 +36,34 @@ class Board:
         self.head[x]+=1
         self.last_player= -self.last_player+3
 
+    def scan(self,x,y,dx,dy,c):
+        #Algorithm by Tor Lattimore
+        count=0
+        while(x<7 and x>-1 and y>-1 and y<6 and self.state[y][x]==c):
+            x+=dx
+            y+=dy
+            count+=1
+        return count
+
     def last_move_won(self):
         if len(self.move_hist)==0:
             return False
         last_y,last_x=self.move_hist[-1]
 
         #horiz
-        x=max(0,last_x-3)
+        if self.scan(last_x-1,last_y,-1,0,self.last_player) + self.scan(last_x+1,last_y,1,0,self.last_player)>=3:
+            return True
+        #vertical
+        if self.scan(last_x,last_y-1,0,-1,self.last_player) + self.scan(last_x,last_y+1,0,1,self.last_player)>=3:
+            return True
+        #diagonal
+        if self.scan(last_x-1,last_y-1,-1,-1,self.last_player) + self.scan(last_x+1,last_y+1,1,1,self.last_player)>=3:
+            return True
+        if self.scan(last_x-1,last_y+1,-1,1,self.last_player) + self.scan(last_x+1,last_y-1,1,-1,self.last_player)>=3:
+            return True
+        return False
 
-        while(x+3<7 and x<=last_x):
-            slc=self.state[last_y][x:x+4]
-            if len(set(slc))==1 and self.last_player in slc:
-                return True
-            x+=1
-        #vert
-        if last_y<=2:
-            slc=[row[last_x] for row in self.state[last_y:last_y+4]]
-            if len(set(slc))==1 and self.last_player in slc:
-                return True
-                    
-        #left diagonal
-        x=last_x-3
-        y=last_y-3
-        while x<0 or y<0:
-            x+=1
-            y+=1
-        while x+3<7 and y+3<6 and x<=last_x and y<=last_y:
-            slc=[self.state[y+i][x+i] for i in range(0,4)]
-            if len(set(slc))==1 and self.last_player in slc:
-                return True
-            x+=1
-            y+=1
-        
-        #right diagonal. 
-        x=last_x+3
-        y=last_y-3
-        while x>6 or y<0:
-            x-=1
-            y+=1
-        while x-3>=0 and y+3<6 and x>=last_x and y<=last_y:
-            slc=[self.state[y+i][x-i] for i in range(0,4)]
-            if len(set(slc))==1 and self.last_player in slc:
-                return True
-            x-=1
-            y+=1
-    
+
     def diagnostic(self,msg=""):
         print(msg)
         print(self)
