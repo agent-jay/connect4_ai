c7921ced7f090d84b4325143635c6592ddfa9ae2
sidjayam <sidjayam@umail.iu.edu>
2016-10-29 20:15:38 -0400

    Player is now compatible with bitboard. Also, getmove has pruning too

--- player.py
+++ player.py
@@ -9,13 +9,13 @@ inf=1000
 class Player:
     def __init__(self):
         self.b=board.Board()
-        self.coin= 2 #It's the coin this player uses on the board
-        self.eval_table=[[3,4,5,7,5,4,3],
-                         [4,6,8,10,8,6,4],
-                         [5,8,11,13,11,8,5],
-                         [5,8,11,13,11,8,5],
-                         [4,6,8,10,8,6,4],
-                         [3,4,5,7,5,4,3]]
+        self.coin= 1 #It's the coin this player uses on the board
+#         self.eval_table=[[3,4,5,7,5,4,3],
+#                          [4,6,8,10,8,6,4],
+#                          [5,8,11,13,11,8,5],
+#                          [5,8,11,13,11,8,5],
+#                          [4,6,8,10,8,6,4],
+#                          [3,4,5,7,5,4,3]]
         self.move_pref= {0:3, 1:2, 2:1, 3:0, 4:1, 5:2, 6:3}
         self.move_pref_dict={}
         self.gen_move_pref_dict()
@@ -53,7 +53,7 @@ class Player:
         #Iterative deepening
         self.start_time=time.time()
         if len(self.b.move_hist)==0:
-            self.coin=1
+            self.coin=0
         self.b.total_moves=0
         best_moves=[]
         depth=1
@@ -72,9 +72,10 @@ class Player:
 
     def get_move_at_depth(self,depth):
         values=[]
+        alpha=-inf
         for move in self.b.generate_moves():
             self.b.make_move(move)
-            value=(self.alpha_beta_minimax(depth-1,-inf,
+            value=(self.alpha_beta_minimax(depth-1,alpha,
                 inf,maxplayer=False), move)
             # values.append((self.minimax(depth-1,maxplayer=False),move))
             self.b.unmake_last_move()
@@ -90,12 +91,12 @@ class Player:
                     best_mv=value[1]
         return best_mv
 
-    def evaluation(self,maxplayer):
-        # return 0
+    def evaluation(self):
+        return board.PLAYER_SIGN[self.coin]*sum(self.b.eval_sum)
         i=len(self.b.move_hist)-1
         evalsum=0
         while(i>=0):
-            y,x=self.b.move_hist[i]
+            x=self.b.move_hist[i]
             if self.b.state[y][x]==self.coin:
                 evalsum+= self.eval_table[y][x]
             else:
@@ -118,11 +119,10 @@ class Player:
             else :
                 return +300
         if depth==0: 
-            return self.evaluation(maxplayer) #evaluation of board
+            return self.evaluation() #evaluation of board
         if self.b.is_full():
             return 0
         
-        # for move in self.move_order(self.b.gen_hashmove()):
         # a=self.b.generate_moves()
         # b=sorted(self.move_order())
         # c=self.b.gen_hashmove() 
