407e3d608a5ede3b615e8a7a65a557e63c2e38cf
sidjayam <sidjayam@umail.iu.edu>
2016-10-29 20:13:42 -0400

    Zobrist hashing now works with bitboard, incremental count of materialvalue- used for eval. Tested

--- board.py
+++ board.py
@@ -1,45 +1,47 @@
 from random import randint,seed
 import numpy as np
 
-#largest num represented by 64bit int. Ensures zobrist key collisions don't happen
-int_64_max=int('1'*64,2)
+INT_64_MAX=int('1'*64,2)
+TOP= int(('1000000'*7).zfill(64), 2)
+TOP_INT=[6,13,20,27,34,41,48]
+PLAYER_SIGN=[1,-1]
 
 class Board:
     def __init__(self):
         self.bitboards= [0,0]
         self.move_hist= []
         self.last_player= 2
-        self.top=[6,13,20,27,34,41,48]
         self.head=[x for x in range(0,43,7)] #position of highest elements
         self.total_moves=0
         self.counter=0
         self.rand_seed=53
-        self.zobrist=[[[0 for x in range(7)] for y in range(6)] for player in range(2)]
+        self.zobrist=[[],[]]
         self.zobrist_key=0
         self.zobrist_init()
+        self.eval_sum=[0,0]
+        self.eval_table=[3,4,5,5,4,3,0,4,6,8,8,6,4,0,5,8,11,11,8,5,0,7,10,
+                13,13,10,7,0,5,8,11,11,8,5,0,4,6,8,8,6,4,0,3,4,5,5,4,3,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     def zobrist_init(self):
         seed(self.rand_seed)
-
-        for player in range(2):
-            for y in range(6):
-                for x in range(7):
-                    self.zobrist[player][y][x]=randint(0,int_64_max)
-        #Is it needed? One more element that represents side. Two positions can be identical
-        #with the only difference being which side is playing
-        # self.zobrist.append(randint(0,1000)) 
+        self.zobrist=[[randint(0,INT_64_MAX) for i in range(64)],
+                [randint(0,INT_64_MAX) for i in range(64)]]
     
     def make_move(self, col):
         #Head-store position of topmost piece on each column. Fast vertical checks
+        player_index= self.counter & 1
         move= 1<<self.head[col]
-        self.bitboards[self.counter & 1]^=move
+        self.bitboards[player_index]^= move
+        
+        self.zobrist_key^=self.zobrist[player_index][self.head[col]]
+        self.eval_sum[player_index]+= PLAYER_SIGN[player_index]*self.eval_table[self.head[col]]
 
         self.head[col]+=1
         self.move_hist.append(col)
         self.counter+=1
         self.last_player=-self.last_player+3
         self.total_moves+=1
-        # self.zobrist_key^=self.zobrist[self.last_player-1][y][x]
         
     def unmake_last_move(self):
         if len(self.move_hist)==0:
@@ -48,25 +50,27 @@ class Board:
         self.head[col]-=1
         move= 1<<self.head[col]
         self.counter-=1
-        self.bitboards[self.counter & 1]^=move
+        player_index= self.counter & 1
+        self.bitboards[player_index]^=move
         
-        # self.zobrist_key^=self.zobrist[self.last_player-1][y][x]
+        self.zobrist_key^=self.zobrist[player_index][self.head[col]]
+        self.eval_sum[player_index]-= PLAYER_SIGN[player_index]*self.eval_table[self.head[col]]
+
         self.last_player= -self.last_player+3
     
     def generate_moves(self):
         #checks if top row of a column(ie. head>0) is empty and returns if so
-        return [i for i,val in enumerate(self.head) if val not in
-                self.top]
+        return [col for col in range(7) if not(TOP & (1<<self.head[col]))]
 
     def gen_hashmove(self):
         key=0
-        for i,val in enumerate(self.head):
-            if val not in self.top:
-                key+= 2**(6-i)
+        for col in range(7):
+            if not (TOP & (1<<self.head[col])):
+                key+= 2**(6-col)
         return key
                 
     def is_full(self):
-        if self.head==self.top:
+        if self.head==TOP_INT:
             return True
         return False
 
