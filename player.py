b673361fcf9849ee2501c10cf1ccee4b4a2b430a
sidjayam <sidjayam@umail.iu.edu>
2016-11-04 20:11:00 -0400

    Looks like I have a working implementation of killer heursitic

--- player.py
+++ player.py
@@ -3,24 +3,20 @@ import random
 # import board
 from . import board #Change this everytime
 from itertools import combinations
+from collections import defaultdict
 
 inf=1000
-
+KILLER_MOVE_LEN=2
 class Player:
     def __init__(self):
         self.b=board.Board()
         self.coin= 1 #It's the coin this player uses on the board
-#         self.eval_table=[[3,4,5,7,5,4,3],
-#                          [4,6,8,10,8,6,4],
-#                          [5,8,11,13,11,8,5],
-#                          [5,8,11,13,11,8,5],
-#                          [4,6,8,10,8,6,4],
-#                          [3,4,5,7,5,4,3]]
         self.move_pref= {0:3, 1:2, 2:1, 3:0, 4:1, 5:2, 6:3}
         self.move_pref_dict={}
         self.gen_move_pref_dict()
         self.move_pref_dict_ab={}
-        self.killer={}
+        self.killer=defaultdict(list)
+        self.history={}
         self.timeout=3
         self.prev_depth=0
         self.depth_limit=43
@@ -36,7 +32,7 @@ class Player:
                 for i in comb:
                     j+=2**(6-i)
                 self.move_pref_dict[j]= sorted(comb,key=lambda x:self.move_pref[x])
-                print(self.move_pref_dict)
+        self.move_pref_dict[0]=[] #When no move is possible
 
     def make_move(self, move):
         self.b.make_move(move)
@@ -46,11 +42,19 @@ class Player:
         move_cd=self.b.gen_hashmove()
         moves= self.move_pref_dict[move_cd]
         best_mv=self.move_pref_dict_ab.get(self.b.zobrist_key)
-        killer_mv=self.killer.get(depth)
-        if killer_mv: 
-            return[killer_mv]+[mv for mv in moves if mv!=killer_mv]
-        if best_mv: 
-            return[best_mv]+[mv for mv in moves if mv!=best_mv]
+        final_moves=[]
+        if best_mv in moves:
+            final_moves.append(best_mv)
+        killer_mv= self.killer.get(depth)
+        if killer_mv:
+            final_moves+=[mv for mv in killer_mv if mv in moves and
+                    mv!=best_mv]
+        if final_moves:
+            return final_moves+[mv for mv in moves if mv not in final_moves]
+        # if killer_mv: 
+            # moves=  killer_mv+[mv for mv in moves if mv not in killer_mv]
+        # if best_mv in moves: 
+            # return [best_mv]+[mv for mv in moves if mv!=best_mv]
         return moves
 
 
@@ -72,16 +76,17 @@ class Player:
         self.prev_depth=depth-1
         print(self.prev_depth)
         self.move_pref_dict_ab={}
-        self.killer={}
+        self.killer.clear()
         return best_moves[-1]
         
 
     def get_move_at_depth(self,depth):
         values=[]
         alpha=-inf
-        for move in self.b.generate_moves():
+        # for move in self.b.generate_moves():
+        for move in self.move_order(100): #arbit large depth.
             self.b.make_move(move)
-            value=(self.alpha_beta_minimax(depth-1,alpha,
+            value=(self.alpha_beta_minimax(0,depth-1,alpha,
                 inf,maxplayer=False), move)
             # values.append((self.minimax(depth-1,maxplayer=False),move))
             self.b.unmake_last_move()
@@ -98,7 +103,7 @@ class Player:
         return best_mv
 
     def evaluation(self):
-        return 0
+        # return 0
         return board.PLAYER_SIGN[self.coin]*sum(self.b.eval_sum)
         i=len(self.b.move_hist)-1
         evalsum=0
@@ -117,7 +122,7 @@ class Player:
         else:
             return +self.eval_table[y][x]
 
-    def alpha_beta_minimax(self,depth, alpha, beta, maxplayer):
+    def alpha_beta_minimax(self,depth,depthleft, alpha, beta, maxplayer):
         if time.time()-self.start_time>= self.timeout:
             return None
         if self.b.last_move_won():
@@ -125,13 +130,13 @@ class Player:
                 return -300
             else :
                 return +300
-        if depth==0: 
+        if depthleft==0: 
             return self.evaluation() #evaluation of board
         if self.b.is_full():
             return 0
         
         # a=self.b.generate_moves()
-        # b=sorted(self.move_order())
+        # b=sorted(self.move_order(depth))
         # c=self.b.gen_hashmove() 
         # if a !=b:
             # print("ERROR")
@@ -142,13 +147,16 @@ class Player:
         best_mv=None
         for move in self.move_order(depth):
             self.b.make_move(move)
-            v= self.alpha_beta_minimax(depth-1, alpha,beta, not maxplayer) 
+            v= self.alpha_beta_minimax(depth+1,depthleft-1, alpha,beta, not maxplayer) 
             self.b.unmake_last_move()
             if v==None:
                 return None
             if maxplayer and v>=beta:
-                self.move_pref_dict_ab[self.b.zobrist_key]=move
-                self.killer[depth]=move
+                # self.move_pref_dict_ab[self.b.zobrist_key]=move
+                tmp=self.killer[depth]
+                tmp.insert(0,move)
+                if len(tmp)>=KILLER_MOVE_LEN:
+                    tmp.pop()
                 return beta
             if maxplayer and v>alpha:
                 alpha=v
